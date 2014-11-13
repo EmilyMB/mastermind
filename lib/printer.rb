658e467a4e15fc05fcc843b51f32d90cca43cca5
Emily Berkeley <eberkeley@gmail.com>
2014-11-13 11:07:19 -0700

    Added printer tests

--- lib/printer.rb
+++ lib/printer.rb
@@ -18,7 +18,13 @@ class Printer
   end
 
   def instructions
-    "The goal of Mastermind is to figure out the color sequence that the computer has generated. For each guess, you enter the sequence of colors that you think the\ncomputer has chosen such as rrbb, which would mean red red blue blue. If the\ncomputer's sequence is bryy, you will be alerted that you have 2 correct colors, 1 of which is in the correct location.\n\nFor additional information about the game, please see the Wikipedia page at\nhttp://en.wikipedia.org/wiki/Mastermind_(board_game)"
+    "The goal of Mastermind is to figure out the color sequence that the computer
+has generated. For each guess, you enter the sequence of colors that you think the
+computer has chosen such as rrbb, which would mean red red blue blue. If the
+computer's sequence is bryy, you will be alerted that you have 2 correct colors,
+1 of which is in the correct location.
+\nFor additional information about the game, please see the Wikipedia page at
+http://en.wikipedia.org/wiki/Mastermind_(board_game)"
   end
 
   def valid_prompt
@@ -26,7 +32,8 @@ class Printer
   end
 
   def time_message(solution, guess_count, minutes, seconds)
-    "Congratulations! You guessed the sequence '#{solution.upcase}' in #{guess_count} guesses over #{minutes} minutes and #{seconds} seconds. Do you want to (p)lay again or (q)uit?"
+    "Congratulations! You guessed the sequence '#{solution.upcase}' in #{guess_count} guesses over #{minutes} minutes,
+#{seconds} seconds. Do you want to (p)lay again or (q)uit?"
   end
 
   def guess_too_long
@@ -50,7 +57,7 @@ class Printer
   end
 
   def correct_place(guess, correct_color, correct_place)
-    "'#{guess.upcase}' has #{correct_color} of the correct elements with #{correct_place} in the correct position"
+    "'#{guess.upcase}' has #{correct_color} of the correct elements with #{correct_place} in the correct position."
   end
 
   def quit_message
--- test/printer_test.rb
+++ test/printer_test.rb
@@ -24,13 +24,15 @@ class PrinterTest < Minitest::Test
 
 
     assert_equal @printer.instructions,
-      "The goal of Mastermind is to figure out the color sequence that the computer has generated. For each guess, you enter the sequence of colors that you think the\ncomputer has chosen such as rrbb, which would mean red red blue blue. If the\ncomputer's sequence is bryy, you will be alerted that you have 2 correct colors, 1 of which is in the correct location.\n\nFor additional information about the game, please see the Wikipedia page at\nhttp://en.wikipedia.org/wiki/Mastermind_(board_game)"
+    "The goal of Mastermind is to figure out the color sequence that the computer
+has generated. For each guess, you enter the sequence of colors that you think the
+computer has chosen such as rrbb, which would mean red red blue blue. If the
+computer's sequence is bryy, you will be alerted that you have 2 correct colors,
+1 of which is in the correct location.
+\nFor additional information about the game, please see the Wikipedia page at
+http://en.wikipedia.org/wiki/Mastermind_(board_game)"
 
     assert_equal @printer.valid_prompt, "please enter a valid choice"
-
-
-    # assert_equal @printer.time_message(solution, guess_count, minutes, seconds),
-    #   "Congratulations! You guessed the sequence '#{solution.upcase}' in #{guess_count} guesses over #{minutes} minutes and #{seconds} seconds. Do you want to (p)lay again or (q)uit?"
 
 
     assert_equal @printer.guess_too_long, "Your guess is too long. Please enter 4 characters."
@@ -44,15 +46,30 @@ class PrinterTest < Minitest::Test
 
     assert_equal @printer.first_guess, "You've taken 1 guess."
 
-    #
-    # assert_equal @printer.guess_count(guess_count),
-    #   "You've taken #{guess_count} guesses"
-    #
-    #
-    # assert_equal @printer.correct_place(guess, correct_color, correct_place),
-    #   "'#{guess.upcase}' has #{correct_color} of the correct elements with #{correct_place} in the correct position"
-    #
-
     assert_equal @printer.quit_message, "Now quitting...off to do something more productive."
   end
+
+  def test_prints_the_guess_count
+    guess_count = 2
+    assert_equal @printer.guess_count(guess_count), "You've taken 2 guesses."
+  end
+
+  def test_prints_correct_count_and_placement
+    guess = "rrrr"
+    correct_color = 3
+    correct_place = 2
+    assert_equal @printer.correct_place(guess, correct_color, correct_place),
+      "'RRRR' has 3 of the correct elements with 2 in the correct position."
+  end
+
+  def test_prints_win_message
+    solution = 'rrrr'
+    guess_count = 2
+    minutes = 2
+    seconds = 3
+    assert_equal @printer.time_message(solution, guess_count, minutes, seconds),
+      "Congratulations! You guessed the sequence 'RRRR' in 2 guesses over 2 minutes,\n3 seconds. Do you want to (p)lay again or (q)uit?"
+  end
+
+
 end
